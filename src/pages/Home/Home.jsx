92c3ec49904f97d11d6c0123a0dbecf68cc44aa8
Arthur Kudabaev <arturo.kudabaev@mail.ru>
2023-07-21 19:23:38 +0500

    Create Store

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,33 +6,41 @@ import {Catalog} from "../../components/Catalog";
 import {About} from "../../components/About";
 import {Delivery} from "../../components/Delivery";
 import {Social} from "../../components/Social";
+import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import {setCategoryId} from "../../redux/slices/categorySlice";
 
 
 export const Home = () => {
 
+    const { categoryId, categorySale } = useSelector(state => state.category)
+    const dispatch = useDispatch();
+
+    const onChangeCategory = (id) => {
+        dispatch(setCategoryId(id))
+    }
+
     const [books, setBooks] = React.useState([]);
     const [sale, setSale] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
-    const [categoryId, setCategoryId] = React.useState(1);
+
 
     React.useEffect(() => {
         setIsLoading(true);
-        fetch('https://library-name.onrender.com/books?categoryId=' + categoryId)
-            .then((res) => res.json())
-            .then((arr) => {
-                setBooks(arr);
+        axios.get('https://library-name.onrender.com/books?categoryId=' + categoryId)
+            .then(response => {
+                setBooks(response.data);
                 setIsLoading(false);
-            });
+            })
     },[categoryId]);
 
     React.useEffect(() => {
-        fetch('https://library-name.onrender.com/books?categoryId=6')
-            .then((res) => res.json())
-            .then((arr) => {
-                setSale(arr);
+        axios.get(`https://library-name.onrender.com/books?categoryId=${categorySale}`)
+            .then(response => {
+                setSale(response.data);
                 setIsLoading(false);
-            });
-    },[]);
+            })
+    },[categorySale]);
 
     return (
         <>
@@ -41,7 +49,7 @@ export const Home = () => {
                 books={books}
                 isLoading={isLoading}
                 categoryId={categoryId}
-                changeCategoryId={(i) => setCategoryId(i)}
+                changeCategoryId={onChangeCategory}
             />
             <Sale books={sale} isLoading={isLoading}/>
             <Catalog />
---
+++ src/redux/slices/categorySlice.js
@@ -0,0 +1,21 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    categoryId: 1,
+    categorySale: 6,
+}
+
+const categorySlice = createSlice({
+    name: 'categories',
+    initialState,
+    reducers: {
+        setCategoryId(state, action) {
+            state.categoryId = action.payload;
+        },
+    }
+});
+
+
+export const { setCategoryId } = categorySlice.actions;
+
+export default categorySlice.reducer;
